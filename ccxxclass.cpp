c53c3430c56975ccfaef0eb96201dc1015d9c65d
Hans-Juergen Lange <hjl@simulated-universe.de>
2026-05-31 12:49:18 +0200

    Fixed mangled qt signal declaration.

--- ccxxclass.cpp
+++ ccxxclass.cpp
@@ -1059,7 +1059,7 @@ void CCxxClass::DumpOperationDecl(std::ostream& hdr, int indent) {
                 hdr << "virtual ";
             }
             hdr << op->GetReturnType(mNameSpace) << " ";
-            hdr << op->name << "(" <<  pdecl << ") ;\n";
+            hdr << op->name << "(" <<  pdecl << ")";
             if (op->isQuery) {
                 hdr << " const";
             }
@@ -1073,6 +1073,7 @@ void CCxxClass::DumpOperationDecl(std::ostream& hdr, int indent) {
                     hdr << " = 0";
                 }
             }
+            hdr << ";\n";
         }
     }
 }
